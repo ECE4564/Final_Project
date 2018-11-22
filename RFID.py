132fdaaf26c9fa0051878f358dc7a0352986dbf6
mpocta30 <40032123+mpocta30@users.noreply.github.com>
2018-11-22 12:32:14 -0500

    Add Web Application files
    Html files inside template for the kiosk display the user interacts with, empy.py controls the web application hosted on a local server on the raspberry pi, RFID.py handles distributing the user's rfid tag to the next available queue client and receiving the success or error message as well as handling the scanning of rfid tags.

---
+++ RFID.py
@@ -0,0 +1,32 @@
+###############################################################################
+##  File:           RFID.py
+##  Author:         Michael Pocta
+##  Date Created:   November 21, 2018
+##  Description:    This file is responsible for calling the RESTful API
+##                  and handling the RFID reader.  Sends RFID tag to the 
+##                  next available Service Pi through RabbitMQ and receives
+##                  the user's name that corresponds to the RFID tag.
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-infobars")
+
+driver = webdriver.Chrome(executable_path="C:\\Users\\Michael\\Documents\\chromedriver.exe", chrome_options=chrome_options)
+driver.fullscreen_window()
+#driver.get("http://localhost:5000/")
+#driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+#sleep(5)
+
+driver.get("http://localhost:5000/login?color=blue&name=Johnny&seat=B4")
+
+sleep(10)
+
+driver.close()
+#driver = webdriver.Chrome("C:\\Users\\Michael\\Documents\\chromedriver.exe")
+
+def read():
+    # Turn on LED with saved intensity values
+    print('Hello')
---
+++ empo.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("main.html")
+
+@app.route("/login")
+def login():
+    # Get request parameters
+    color = request.args.get('color')
+    name = request.args.get('name')
+    seat = request.args.get('seat')
+
+
+    return render_template("login.html", color=color, name=name)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
