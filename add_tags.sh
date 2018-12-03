1995710693b9f1c36e271ab6bfc278857f43649e
James D Toussaint <jdt13@vt.edu>
2018-12-02 19:35:49 -0500

    Added names to db objects

--- add_tags.sh
+++ add_tags.sh
@@ -5,12 +5,12 @@ PORT=5000
 
 curl --header "Content-Type: application/json" \
 	 --request POST \
-	 --data "{\"Tag\": \"165.177.144.27\", \"Status\": \"0\"}" \
+	 --data "{\"Tag\": \"165.177.144.27\", \"Name\": \"Michael Pocta\", \"Status\": \"0\"}" \
 	 http://$IP:$PORT/add_user
 echo ""
 
 curl --header "Content-Type: application/json" \
 	 --request POST \
-	 --data "{\"Tag\": \"126.227.175.133\", \"Status\": \"0\"}" \
+	 --data "{\"Tag\": \"126.227.175.133\", \"Name\": \"David Toussaint\", \"Status\": \"0\"}" \
 	 http://$IP:$PORT/add_user
 echo ""
