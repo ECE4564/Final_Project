1c9663424e7500313a274b96411fd307517dbdab
James D Toussaint <jdt13@vt.edu>
2018-12-02 19:20:07 -0500

    Updated address

--- add_tags.sh
+++ add_tags.sh
@@ -6,5 +6,5 @@ PORT=5000
 curl --header "Content-Type: application/json" \
 	 --request POST \
 	 --data "{\"Tag\": \"165.177.144.27\", \"Status\": \"0\"}" \
-	 http://$IP:$PORT/add
+	 http://$IP:$PORT/add_user
 echo ""
